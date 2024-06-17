6a31bf48bb9d78ed1b1dcb5d2f017838ebf891d4
Matt <mathew.beldon@hotmail.co.uk>
2024-06-17 17:50:01 +0100

    Phase 3

--- Program.cs
+++ Program.cs
@@ -23,7 +23,20 @@ class Program
 
                 if (double.TryParse(Console.ReadLine(), out double itemPrice)) 
                 {
-                    itemList.Add(new Item(itemName, itemPrice));
+                    while (true)
+                    {
+                        Console.WriteLine("Quantity of items: ");
+
+                        if (int.TryParse(Console.ReadLine(), out int itemQuantity)) 
+                        {
+                            itemList.Add(new Item(itemName, itemPrice, itemQuantity));
+
+                            break;
+                        }
+                        
+                        Console.WriteLine("Invalid quantity. Please enter a valid number.");
+                    }
+
                     break;
                 }
                 
@@ -31,37 +44,72 @@ class Program
             }
             
             Console.WriteLine("Would you like to input another item? (y/n)");
+            
             var userResponse  = Console.ReadLine()!;
 
-            if (userResponse == "n") 
+            if (userResponse != "y") 
             {
                 break;
             }
         }
 
-        Console.WriteLine($"Table Number: {tableNumber}");
-        Console.WriteLine($"Server Id: {serverId}");
-
-        var totalPrice = 0.0;
-
+        var tipSubtotal = 0.0;
+        var itemsSubtotal = 0.0;
+        
         foreach(var item in itemList)
         {
-            Console.WriteLine($"Item: {item.Name} Price: {item.Price}");
-            totalPrice += item.{item.Price};
+            itemsSubtotal += item.Price * item.Quantity;
         }
 
-        Console.WriteLine($"Total: {totalPrice}");
+        Console.WriteLine("Would you like to add a tip? (y/n)");
+        var addTip  = Console.ReadLine()!;
+
+        if (addTip == "y") 
+        {
+            while (true)
+            {
+                Console.WriteLine("Tip percentage: ");
+
+                if (int.TryParse(Console.ReadLine(), out int tipPercentage)) 
+                {
+                    tipSubtotal += (itemsSubtotal / 100) * tipPercentage;
+
+                    break;
+                }
+                
+                Console.WriteLine("Invalid tip amount. Please enter a valid number.");
+            }           
+        }
+
+        
+        foreach(var item in itemList)
+        {
+            Console.WriteLine($"Item: {item.Name} Price: {item.Price} Quantity: {item.Quantity}");
+        }
+
+        Console.WriteLine($"");
+        Console.WriteLine($"*********************");
+        Console.WriteLine($"Table Number: {tableNumber}");
+        Console.WriteLine($"Server Id: {serverId}");
+        Console.WriteLine($"");
+        Console.WriteLine($"Subtotal: {itemsSubtotal}");
+        Console.WriteLine($"Tip: {tipSubtotal}");
+        Console.WriteLine($"");
+        Console.WriteLine($"Total: {itemsSubtotal + tipSubtotal}");
+        Console.WriteLine($"*********************");
     }
 }
 
 class Item
 {
-    public Item(string name, double price)
+    public Item(string name, double price, int quantity)
     {
         Name = name;
         Price = price;
+        Quantity = quantity;
     }
 
     public string Name {get; set;}
     public double Price {get; set;}
+    public int Quantity {get; set;}
 }
