38cfd6e68273353847b00755986e758c56e4f33e
Matt <mathew.beldon@hotmail.co.uk>
2024-06-18 16:27:50 +0100

    Phase 5

--- Program.cs
+++ Program.cs
@@ -6,122 +6,150 @@ class Program
 {
     static void Main()
     {
-        var itemMenu = new List<Item>()
+        var hasSelectedExit = false;
+        while (!hasSelectedExit) 
         {
-            new Item(1, "item 1", 5.99),
-            new Item(2, "item 2", 1.99),
-            new Item(3, "item 3", 0.99),
-            new Item(4, "item 4", 1.50),
-            new Item(5, "item 5", 3.50)
-        };
-
-        var itemList = new Dictionary<Item, int>();
-
-        Console.WriteLine("Enter your table number: ");
-        var tableNumber  = Console.ReadLine()!;
-
-        Console.WriteLine("Enter your server ID: ");
-        var serverId = Console.ReadLine()!;
-
-        while (true) 
-        {
-            foreach (var item in itemMenu)
-            {
-                Console.WriteLine($"{item.ItemId}. {item.Name} - £{item.Price:0.00}");
-            }
+            Console.WriteLine("1. Start Order");
+            Console.WriteLine("2. Exit");
 
             while (true)
             {
-                Console.WriteLine("Select your item: ");
-                var menuSelection  = Console.ReadLine()!;
+                Console.WriteLine("Menu choice: ");
+                var mainMenuSelection  = Console.ReadLine()!;
 
-                if (!int.TryParse(menuSelection, out int selectedItem)){
-                    Console.WriteLine("Please enter a valid number.");
-                    continue;
+                if (mainMenuSelection == "1")
+                {
+                    var order = StartOrder();
+                    PrintRecipt(order);
+                    break;
                 }
 
-                var foundItem = itemMenu.SingleOrDefault(x => x.ItemId == selectedItem);
-                if (foundItem is not null)
+                if (mainMenuSelection == "2")
                 {
-                    while (true)
-                    {
-                        Console.WriteLine("Quantity of items: ");
+                    hasSelectedExit = true;
+                    break;
+                }
+            }
+        }
 
-                        if (int.TryParse(Console.ReadLine(), out int itemQuantity)) 
+        Order StartOrder()
+        {
+            var itemMenu = new List<Item>()
+            {
+                new Item(1, "item 1", 5.99),
+                new Item(2, "item 2", 1.99),
+                new Item(3, "item 3", 0.99),
+                new Item(4, "item 4", 1.50),
+                new Item(5, "item 5", 3.50)
+            };
+
+            Console.WriteLine("Enter your table number: ");
+            var tableNumber  = Console.ReadLine()!;
+
+            Console.WriteLine("Enter your server ID: ");
+            var serverId = Console.ReadLine()!;
+
+            var order = new Order(tableNumber, serverId);
+
+            while (true) 
+            {
+                foreach (var item in itemMenu)
+                {
+                    Console.WriteLine($"{item.ItemId}. {item.Name} - £{item.Price:0.00}");
+                }
+
+                while (true)
+                {
+                    Console.WriteLine("Select your item: ");
+                    var menuSelection  = Console.ReadLine()!;
+
+                    if (!int.TryParse(menuSelection, out int selectedItem))
+                    {
+                        Console.WriteLine("Please enter a valid number.");
+                        continue;
+                    }
+
+                    var foundItem = itemMenu.SingleOrDefault(x => x.ItemId == selectedItem);
+                    if (foundItem is not null)
+                    {
+                        while (true)
                         {
-                            if (itemList.ContainsKey(foundItem))
+                            Console.WriteLine("Quantity of items: ");
+
+                            if (int.TryParse(Console.ReadLine(), out int itemQuantity)) 
                             {
-                                itemList[foundItem] += itemQuantity;
+                                if (order.OrderItems.ContainsKey(foundItem))
+                                {
+                                    order.OrderItems[foundItem] += itemQuantity;
+
+                                    break;
+                                }
+
+                                order.OrderItems.Add(foundItem, itemQuantity);
 
                                 break;
                             }
-
-                            itemList.Add(foundItem, itemQuantity);
-
-                            break;
+                            
+                            Console.WriteLine("Invalid quantity. Please enter a valid number.");
                         }
-                        
-                        Console.WriteLine("Invalid quantity. Please enter a valid number.");
+
+                        break;
                     }
-
-                    break;
+                    
+                    Console.WriteLine("Invalid item Id. Please enter a valid Id.");
                 }
                 
-                Console.WriteLine("Invalid price. Please enter a valid number.");
-            }
-            
-            Console.WriteLine("Would you like to input another item? (y/n)");
-            
-            var userResponse  = Console.ReadLine()!;
-            if (userResponse != "y") 
-            {
-                break;
-            }
-        }
-
-        var tipSubtotal = 0.0;
-        var itemsSubtotal = 0.0;
-        
-        foreach(var item in itemList)
-        {
-            itemsSubtotal += item.Key.Price * item.Value;
-        }
-
-        Console.WriteLine("Would you like to add a tip? (y/n)");
-        var addTip  = Console.ReadLine()!;
-
-        if (addTip == "y") 
-        {
-            while (true)
-            {
-                Console.WriteLine("Tip percentage: ");
-
-                if (int.TryParse(Console.ReadLine(), out int tipPercentage)) 
+                Console.WriteLine("Would you like to input another item? (y/n)");
+                
+                var userResponse  = Console.ReadLine()!;
+                if (userResponse != "y") 
                 {
-                    tipSubtotal += (itemsSubtotal / 100) * tipPercentage;
-
                     break;
                 }
-                
-                Console.WriteLine("Invalid tip amount. Please enter a valid number.");
-            }           
+            }
+
+            Console.WriteLine("Would you like to add a tip? (y/n)");
+            var addTip  = Console.ReadLine()!;
+
+            if (addTip == "y") 
+            {
+                while (true)
+                {
+                    Console.WriteLine("Tip percentage: ");
+
+                    if (int.TryParse(Console.ReadLine(), out int tipPercentage)) 
+                    {
+                        order.TipAmount = tipPercentage;
+
+                        break;
+                    }
+                    
+                    Console.WriteLine("Invalid tip amount. Please enter a valid number.");
+                }           
+            }
+
+            return order;
         }
 
-        Console.WriteLine($"");
-        Console.WriteLine($"*********************");
-        Console.WriteLine($"Table Number: {tableNumber}");
-        Console.WriteLine($"Server Id: {serverId}");
-        Console.WriteLine($"");
-        foreach(var item in itemList)
+        void PrintRecipt(Order order)
         {
-            Console.WriteLine($"Item: {item.Key.Name} Price: {item.Key.Price} Quantity: {item.Value}");
+            Console.WriteLine("");
+            Console.WriteLine("*********************");
+            Console.WriteLine($"Table Number: {order.TableNumber}");
+            Console.WriteLine($"Server Id: {order.ServerId}");
+            Console.WriteLine("");
+            foreach(var item in order.OrderItems)
+            {
+                Console.WriteLine($"Item: {item.Key.Name} Price: {item.Key.Price} Quantity: {item.Value}");
+            }
+            Console.WriteLine("");     
+            Console.WriteLine($"Subtotal: {order.GetSubtotal()}");
+            Console.WriteLine($"Tip: {order.GetTipAmount()}");
+            Console.WriteLine("");
+            Console.WriteLine($"Total: {order.GetSubtotal() + order.GetTipAmount()}");
+            Console.WriteLine("*********************");
+            Console.WriteLine("");     
         }
-        Console.WriteLine($"");     
-        Console.WriteLine($"Subtotal: {itemsSubtotal}");
-        Console.WriteLine($"Tip: {tipSubtotal}");
-        Console.WriteLine($"");
-        Console.WriteLine($"Total: {itemsSubtotal + tipSubtotal}");
-        Console.WriteLine($"*********************");
     }
 }
 
@@ -137,4 +165,37 @@ class Item
     public int ItemId {get; set;}
     public string Name {get; set;}
     public double Price {get; set;}
+}
+
+class Order
+{
+    public Order(string tableNumber, string serverId)
+    {
+        TableNumber = tableNumber;
+        ServerId = serverId;
+        OrderItems = new();
+    }
+
+    public string TableNumber {get; set;}
+    public string ServerId {get; set;}
+
+    public int TipAmount {get; set; }
+    public Dictionary<Item, int> OrderItems {get; set;}
+
+    public double GetSubtotal()
+    {
+        var subtotal = 0.0;
+
+        foreach(var item in OrderItems)
+        {
+            subtotal += item.Key.Price * item.Value;
+        }
+
+        return subtotal;
+    }
+
+    public double GetTipAmount()
+    {
+        return (GetSubtotal() / 100) * TipAmount;
+    }
 }
